806d8b2f7a98c8573a1e3f69fe7658055080dfdb
antoine.vinot <antoine.vinot@sonarsource.com>
2024-10-09 15:47:33 +0200

    This is a test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
   public static void main(String[] args) {
+    printHelloWorld();
+  }
+
+  public static void printHelloWorld() {
     System.out.println("Hello world!");
+
+    System.out.println("This is a test.");
   }
 }
